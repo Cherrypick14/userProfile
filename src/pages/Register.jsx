5f9f0d486abaa714e5b4f9eaab0395c41d9134aa
Cherrypick14 <cherrypicklet@gmail.com>
2022-05-12 10:43:16 +0300

    add onSubmission

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,10 +3,14 @@ import '../styles/register.css'
 import { Link } from 'react-router-dom'
 
 const Register = () => {
+   const onSubmit =(e)=>{
+    e.preventDefault();
+    console.log("successful submission")
+   }
   return (
     <div className='register'>
     <span className="registerTitle">create account</span>
-   <div className="registerform">
+   <form className="registerform" onSubmit={onSubmit}>
         <label>NAME</label>
         <input type="text" className="registerinput" placeholder="your name.."/>
         <label>EMAIL</label>
@@ -17,7 +21,7 @@ const Register = () => {
         <input type="password"className="registerinput" placeholder="Repeat your password..."/>
 
         <button className="registerButton">Sign Up</button>
-   </div>
+   </form>
   <div className='moreInfo'>
       <span>Have an account?</span>
      <Link className="link"to ="/login">Login here</Link>
