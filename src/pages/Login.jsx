a598b89f7737921dd79d38f2f45b4974cc45e490
Cherrypick14 <cherrypicklet@gmail.com>
2022-05-12 10:43:02 +0300

    add onsubmission

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,16 +3,20 @@ import '../styles/login.css'
 import { Link } from 'react-router-dom'
 
 const Login = () => {
+  const onSubmit =(e)=>{
+    e.preventDefault();
+    console.log("successful submission")
+   }
   return (
     <div className='login'>
           <span className="loginTitle">Welcome to future.me</span>
-         <div className="loginform">
+         <form className="loginform" onSubmit={onSubmit}>
               <label>Email</label>
               <input type="email" className="logininput" placeholder="enter your email.."/>
               <label>Password</label>
               <input type="password"className="logininput" placeholder="enter your password..."/>
               <button className="loginButton">Login</button>
-         </div>
+         </form>
           
           <div className='moreinfo2'>
               <Link className="link"to="/register">Create Account</Link>
