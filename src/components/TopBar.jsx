8ceefad2e0dac7a9f06391c666c374b535eb31bb
Cherrypick14 <cherrypicklet@gmail.com>
2022-05-14 14:51:38 +0300

    remove blog link

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -3,7 +3,7 @@ import '../styles/topbar.css'
 import {Link} from "react-router-dom"
 
 const TopBar = () => {
-   const user = false;
+   const user = true;
   return (
     <div className='top'>
       <div className="topLeft">
