fec94facccf6f09c880aa74e5ef0a13c39c77cf8
Cherrypick14 <cherrypicklet@gmail.com>
2022-05-16 07:57:04 +0300

    change user to false

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -3,7 +3,7 @@ import '../styles/topbar.css'
 import {Link} from "react-router-dom"
 
 const TopBar = () => {
-   const user = true;
+   const user = false;
   return (
     <div className='top'>
       <div className="topLeft">
